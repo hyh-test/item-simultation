fbbc944f34f8a287af73a360244a9f2689a33105
황종하 <whdgk3615@naver.com>
2024-11-27 16:53:39 +0900

    [fix] invetory get
    인벤토리 조회 기능에서 아이템 이름 나오게 수정

--- routes/inventory.router.js
+++ routes/inventory.router.js
@@ -7,45 +7,47 @@ dotenv.config();
 
 const router = express.Router();
 
-
+//인벤토리 조회
 router.get("/:characterId", authMiddleware, async (req, res, next) => {
-    try {
-      const { characterId } = req.params; // URL 파라미터로 전달된 캐릭터 ID
-      const { id: userId } = req.user; // 로그인한 유저의 ID (authMiddleware에서 제공)
-  
-      // 캐릭터를 찾고, 해당 캐릭터가 로그인한 유저의 것인지 확인
-      const character = await prisma.character.findFirst({
-        where: {
-          id: +characterId, // 캐릭터 ID가 요청된 ID와 일치하는지
-        },
-      });
-  
-      // 캐릭터가 존재하지 않으면 오류 반환
-      if (!character) {
-        const error = new Error("해당 캐릭터를 찾을 수 없습니다.");
-        error.status = 404; // Not Found
-        return next(error);
-      }
-  
-      // 캐릭터가 로그인한 유저의 것인지 확인
-      const isOwner = character.userId === userId;
-  
-      // 캐릭터 정보 조회
-      const inventoryData = await prisma.inventory.findFirst({
-        where: {
-          id: +characterId, // 캐릭터 ID가 요청된 캐릭터 ID와 일치하는지
-        },
-        select: {
-          id: true,
-          itemId: true, //아이템 아이디
-          quantity: true, //아이템 수량
-        },
-      });
-  
-      return res.status(200).json({ data: inventoryData });
-    } catch (error) {
-      next(error);
+  try {
+    const { characterId } = req.params; // URL 파라미터로 전달된 캐릭터 ID
+    const { id: userId } = req.user; // 로그인한 유저의 ID (authMiddleware에서 제공)
+
+    // 캐릭터를 찾고, 해당 캐릭터가 로그인한 유저의 것인지 확인
+    const character = await prisma.character.findFirst({
+      where: {
+        id: +characterId, // 캐릭터 ID가 요청된 ID와 일치하는지
+        userId: userId, // 로그인한 유저의 ID와 일치하는지 확인
+      },
+    });
+
+    // 캐릭터가 존재하지 않으면 오류 반환
+    if (!character) {
+      const error = new Error("해당 캐릭터를 찾을 수 없습니다.");
+      error.status = 404; // Not Found
+      return next(error);
     }
-  });
+
+    // 캐릭터의 인벤토리 조회
+    const inventoryData = await prisma.inventory.findMany({
+      where: {
+        characterId: +characterId, // 캐릭터의 ID와 일치하는 인벤토리 데이터 조회
+      },
+      select: {
+        id: true,
+        item: {
+          select: {
+            name: true,
+          },
+        },
+        quantity: true, //아이템 수량
+      },
+    });
+
+    return res.status(200).json({ data: inventoryData });
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default router;
